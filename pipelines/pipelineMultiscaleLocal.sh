f26610df6fd33f91181071bb9486bdf985d60bc1
Stefan Geisselsoeder <stefan.geisselsoeder@fau.de>
2017-03-03 19:19:50 +0100

    Added submit script for local pipeline; fixed issue with the local multiscale pipeline not working properly when submitted to cluster; generalized the logfile pattern in gitignore

--- pipelines/pipelineMultiscaleLocal.sh
+++ pipelines/pipelineMultiscaleLocal.sh
@@ -5,6 +5,8 @@ echo "#### Please make sure that you execute this pipeline scripts from within t
 	namely via ./pipelineMultiscaleLocal.sh, as the relative paths are set accordingly."
 cd ..
 
+module load boost
+
 echo "Make sure the binaries are up-to-date"
 make
 
@@ -13,15 +15,15 @@ echo "#### Evaluating data from" ${inputFile}
 cp ./${inputFile} ./input/events.txt
 
 echo "#### Compute the baseline expectation"
-# time ./bin/step1 --input ./input/events.txt
+# time ./bin/step1 --input "./input/events.txt"
 
 echo "#### Compute pseudo experiments to (in the end) deduce the significance"
-for i in {1..10}; do echo "#### Iteration " $i " of 10" && ./bin/step2 --input ./input/events.txt ; done
+for i in {1..10}; do echo "#### Iteration " $i " of 10" && ./bin/step2 --input "./input/events.txt" ; done
 
 echo "#### Extract the distributions from the pseudo experiments"
-time ./bin/step4 
+time ./bin/step4
 
 echo "#### Process the actual data, find clusters and deduce their significance compared to pseudo experiments"
-time ./bin/step5 --input ./input/events.txt
+time ./bin/step5 --input "./input/events.txt"
 
 echo "#### Multiscale source search has finished. Thank you for your patience. You can find the results in the logfiles and in the subfolder intermediate/step5/."
