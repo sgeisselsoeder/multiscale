916424cb1a86b70b8a5b30d4c6e6a494c1f5ce58
Stefan Geisselsoeder <stefan.geisselsoeder@fau.de>
2017-03-06 18:54:20 +0100

    Added gnuplot output to local input evaluation pipeline

--- pipelines/pipelineLocalInputEvaluation.sh
+++ pipelines/pipelineLocalInputEvaluation.sh
@@ -5,3 +5,10 @@ time ./bin/step1 --input ./input/events.txt
 
 # Evaluate the input distribution against the expectation
 time ./bin/evaluateToSkymap --input ./input/events.txt
+
+cd ./output/
+cp plotscript scenarioEvaluation/
+cp gridLines.grid scenarioEvaluation/
+cd scenarioEvaluation/
+gnuplot plotscript
+
